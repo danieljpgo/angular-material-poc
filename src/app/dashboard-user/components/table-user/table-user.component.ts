909a8d68fb1fbadbad963a95f695d22bc2c360e5
Daniel Jorge <djpgonzaga@gmail.com>
2019-04-24 02:33:29 -0300

    add table from @angular/material

--- src/app/dashboard-user/components/table-user/table-user.component.ts
+++ src/app/dashboard-user/components/table-user/table-user.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+// AfterViewInit
+import { OnInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator, MatSort } from '@angular/material';
+import { TableUserDataSource } from './table-user-datasource';
 
 @Component({
   selector: 'app-table-user',
   templateUrl: './table-user.component.html',
-  styleUrls: ['./table-user.component.scss']
+  styleUrls: ['./table-user.component.css']
 })
-export class TableUserComponent implements OnInit {
 
-  constructor() { }
+// AfterViewInit
+export class TableUserComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  dataSource: TableUserDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['picture', 'name', 'email'];
 
   ngOnInit() {
+    this.dataSource = new TableUserDataSource(this.paginator, this.sort);
   }
-
+  // @TODO Verificar qual o melhor para se usar
+  // ngAfterViewInit() {
+  //   this.dataSource = new TableUserDataSource(this.paginator, this.sort);
+  // }
 }
