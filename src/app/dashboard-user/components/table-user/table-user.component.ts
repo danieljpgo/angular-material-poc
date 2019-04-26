5ee6d1c44f29773ea9e62174895c0834ba99014c
danieljpgo <djpgonzaga@hotmail.com>
2019-04-26 01:27:35 -0300

    sort of material table now working with http request service

--- src/app/dashboard-user/components/table-user/table-user.component.ts
+++ src/app/dashboard-user/components/table-user/table-user.component.ts
@@ -1,11 +1,13 @@
 import { OnInit, AfterViewInit, Component, ViewChild, Output, EventEmitter} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { MatPaginator, MatSort } from '@angular/material';
+
 import { TableUserDataSource } from './table-user-datasource';
+import { DashboardUserService } from '../../dashboard-user.service';
 
 import { User } from '../../models/user.interface';
-import { DashboardUserService } from '../../dashboard-user.service';
-import { ActivatedRoute } from '@angular/router';
-import { tap } from 'rxjs/operators';
+import {tap} from 'rxjs/operators';
+import { merge } from 'rxjs';
 
 @Component({
   selector: 'app-table-user',
@@ -13,17 +15,13 @@ import { tap } from 'rxjs/operators';
   styleUrls: ['./table-user.component.css']
 })
 
-// AfterViewInit
 export class TableUserComponent implements OnInit, AfterViewInit {
-  // @ViewChild(MatSort) sort: MatSort;
 
   @Output() view: EventEmitter<User> = new EventEmitter<User>();
 
-
-
+  @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  // course: User;
   dataSource: TableUserDataSource;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
@@ -32,22 +30,25 @@ export class TableUserComponent implements OnInit, AfterViewInit {
   constructor(private userService: DashboardUserService, private route: ActivatedRoute) {}
 
   ngOnInit() {
-    // this.paginator, this.sort
-    // this.course = this.route.snapshot.data["users"];
     this.dataSource = new TableUserDataSource(this.userService);
-    this.dataSource.loadLessons('', 'asc', 0, 3);
+    this.dataSource.loadLessons('', 'asc', 1, 3);
   }
 
-  // this.dataSource = new TableUserDataSource(this.paginator, this.sort);
-
   ngAfterViewInit() {
+
     this.paginator.page
+      .pipe(tap(() => this.loadLessonsPage()))
+      .subscribe();
+
+    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 1);
+
+    merge(this.sort.sortChange, this.paginator.page)
       .pipe(tap(() => this.loadLessonsPage()))
       .subscribe();
   }
 
   loadLessonsPage() {
-    this.dataSource.loadLessons('', 'asc', this.paginator.pageIndex, this.paginator.pageSize);
+    this.dataSource.loadLessons(this.sort.active, this.sort.direction, this.paginator.pageIndex, this.paginator.pageSize);
   }
 
   editUser(id) {
