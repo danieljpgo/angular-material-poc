3f7502cd85052c2368ffdab24a3b32994921a1ac
danieljpgo <djpgonzaga@hotmail.com>
2019-04-26 02:45:21 -0300

    clear code and removing duplication request

--- src/app/dashboard-user/components/table-user/table-user.component.ts
+++ src/app/dashboard-user/components/table-user/table-user.component.ts
@@ -1,12 +1,12 @@
-import {OnInit, AfterViewInit, Component, ViewChild, Output, EventEmitter, ElementRef} from '@angular/core';
+import { OnInit, AfterViewInit, Component, ViewChild, Output, EventEmitter, ElementRef } from '@angular/core';
 import { MatPaginator, MatSort } from '@angular/material';
 
 import { TableUserDataSource } from './table-user-datasource';
 import { DashboardUserService } from '../../dashboard-user.service';
 
 import { User } from '../../models/user.interface';
-import {debounceTime, distinctUntilChanged, tap} from 'rxjs/operators';
-import {fromEvent, merge} from 'rxjs';
+import { debounceTime, distinctUntilChanged, tap} from 'rxjs/operators';
+import { fromEvent, merge } from 'rxjs';
 
 @Component({
   selector: 'app-table-user',
@@ -18,12 +18,14 @@ export class TableUserComponent implements OnInit, AfterViewInit {
 
   @Output() view: EventEmitter<User> = new EventEmitter<User>();
 
+  /** Sort  */
   @ViewChild(MatSort) sort: MatSort;
+  /** Paginator  */
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  /** Search Input  */
   @ViewChild('inputSearch') inputSearch: ElementRef;
-
+  /** Table Data Source */
   dataSource: TableUserDataSource;
-
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['id', 'name', 'email', 'departament', 'actions'];
 
@@ -31,21 +33,16 @@ export class TableUserComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.dataSource = new TableUserDataSource(this.userService);
-    this.dataSource.loadLessons('', '', 'asc', 0, 5);
+    this.dataSource.loadUser('', '', 'asc', 0, 5);
   }
 
   ngAfterViewInit() {
-
-    this.paginator.page
-      .pipe(tap(() => this.loadLessonsPage()))
-      .subscribe();
-
     /** Reset the paginator after sorting */
     this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
 
     /** On sort or paginate events, load a new page */
     merge(this.sort.sortChange, this.paginator.page)
-      .pipe(tap(() => this.loadLessonsPage()))
+      .pipe(tap(() => this.loadUserTable()))
       .subscribe();
 
     /** Server-side search */
@@ -55,14 +52,14 @@ export class TableUserComponent implements OnInit, AfterViewInit {
         distinctUntilChanged(),
         tap(() => {
           this.paginator.pageIndex = 0;
-          this.loadLessonsPage();
+          this.loadUserTable();
         })
       )
       .subscribe();
   }
 
-  loadLessonsPage() {
-    this.dataSource.loadLessons(
+  loadUserTable() {
+    this.dataSource.loadUser(
       this.inputSearch.nativeElement.value,
       this.sort.active,
       this.sort.direction,
--- src/app/dashboard-user/containers/list-user/list-user.component.ts
+++ src/app/dashboard-user/containers/list-user/list-user.component.ts
@@ -1,34 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { DashboardUserService } from '../../dashboard-user.service';
-
-import { User } from '../../models/user.interface';
-
 @Component({
   selector: 'app-list-user',
   templateUrl: './list-user.component.html',
   styleUrls: ['./list-user.component.scss']
 })
 export class ListUserComponent implements OnInit {
+  constructor(private router: Router) { }
 
-  users: User[] = [];
-
-  constructor(private router: Router, private usersService: DashboardUserService) { }
-
-  ngOnInit() {
-    // this.handleGetUsers();
-  }
+  ngOnInit() {  }
 
   handleView(id) {
     this.router.navigate(['/users', id]);
   }
-
-  // handleGetUsers() {
-  //   // this.usersService.getUsers().subscribe((data: User[]) => {
-  //   //   console.log(data);
-  //   //   return this.users = data;
-  //   // });
-  // }
-
 }
