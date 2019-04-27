9f5732c66fc10bbfa55cd92e6b4da18b0aa18912
Daniel Jorge <djpgonzaga@gmail.com>
2019-04-27 17:27:16 -0300

    add custom-theme for the project

--- src/app/dashboard-user/components/table-user/table-user.component.ts
+++ src/app/dashboard-user/components/table-user/table-user.component.ts
@@ -11,7 +11,7 @@ import { fromEvent, merge } from 'rxjs';
 @Component({
   selector: 'app-table-user',
   templateUrl: './table-user.component.html',
-  styleUrls: ['./table-user.component.css']
+  styleUrls: ['./table-user.component.scss']
 })
 
 export class TableUserComponent implements OnInit, AfterViewInit {
